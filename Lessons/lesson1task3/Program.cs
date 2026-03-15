521c50d8469c5945bd56a45d17dc631a3e5a6822
W1sCont <tmrostik@gmail.com>
2026-03-15 22:20:54 +0100

    lesson 1 task 3

---
+++ Lessons/lesson1task3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CSharpApplication.DataTypes
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                Console.WriteLine("Введіть число для перевірки полідрому");
+                string? str = Console.ReadLine();
+                uint number = Convert.ToUInt32(str);
+
+                uint temp = number;
+                uint rev = 0;
+
+                while (temp > 0)
+                {
+                    uint lastNum = temp % 10;
+                    rev = (rev * 10) + lastNum;
+                    temp /= 10;
+                }
+
+                if (number == rev)
+                {
+                    Console.WriteLine($"Число {number} поліндром");
+                }
+                else
+                {
+                    Console.WriteLine($"Число {number} не поліндром");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
