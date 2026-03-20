580a9f50959faafd069f26c53ef3b937f2cee78b
W1sCont <tmrostik@gmail.com>
2026-03-20 18:49:56 +0100

    lesson 3 task 1 2 3
    Practice

---
+++ Lessons/lesson3task1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task1
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                const ushort len = 20;
+                int[] arr = new int[len];
+                Random r = new();
+                ushort countEven = 0;
+                ushort countOdd = 0;
+                ushort countUniq = 0;
+
+                for (ushort i = 0; i < len; ++i)
+                {
+                    arr[i] = r.Next(1, 20);
+                    if (arr[i] % 2 == 0) countEven++;
+                    else countOdd++;   
+                    Console.Write(arr[i] + " ");
+                }
+
+                Array.Sort(arr);
+
+                for (ushort i = 0; i < len; ++i)
+                {
+                    bool isUniq = true;
+                    if (i > 0 && arr[i] == arr[i - 1]) isUniq = false;
+                    if (i < len - 1 && arr[i] == arr[i + 1]) isUniq = false;
+                    if (isUniq) countUniq++;
+                }
+                
+
+                Console.WriteLine($"\nК-сть парних чисел: {countEven} \nК-сть непарних: {countOdd}\n" +
+                    $"К-сть унікальних чисел: {countUniq}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
---
+++ Lessons/lesson3task3/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Task3
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                const ushort len = 5;
+                int[,] arr = new int[len, len];
+                Random r = new();
+                for (ushort i = 0; i < len; ++i)
+                {
+                    for (ushort j = 0; j < len; ++j)
+                    {
+                        arr[i, j] = r.Next(-10, 40);
+                        Console.Write("{0,4}", arr[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+
+
+                for (ushort j = 0; j < len; ++j)
+                {
+                    int sum = 0;
+                    for (ushort i = 0; i < len; ++i)
+                    {
+                        // Console.Write(arr[i, j] + "     ");
+                        if (arr[i, j] < 0)
+                        {
+                            sum = 0;
+                            break;
+                        }
+                        sum += arr[i, j];
+                    }
+                    if (sum > 0) Console.Write("{0,4}", sum);
+                    else Console.Write("{0,4}", " ");
+                }
+                Console.WriteLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
