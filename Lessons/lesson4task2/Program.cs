a081492a333681c782047b36e74576c0de3ff0fe
W1sCont <tmrostik@gmail.com>
2026-03-22 21:20:52 +0100

    lesson 4 task 2

---
+++ Lessons/lesson4task2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task2
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                Console.WriteLine("Введіть текст: ");
+                string? str = Console.ReadLine();
+                // today is a good day for walking. i will try to walk near the see.
+
+                StringBuilder str1 = new StringBuilder(str);
+                str1[0] = char.ToUpper(str1[0]);
+
+                for(ushort i = 0; i < str1.Length; ++i)
+                {
+                    if (str1[i] == '.' && i != str1.Length - 1)
+                        if (str1[i + 1] == ' ') str1[i + 2] = char.ToUpper(str1[i + 2]);
+                        else str1[i + 1] = char.ToUpper(str1[i + 1]);
+                }
+
+                Console.WriteLine("Результат: ");
+                Console.WriteLine(str1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
