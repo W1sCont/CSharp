02b2143c62b330c40315e9266c9b8eee719ca4b4
W1sCont <tmrostik@gmail.com>
2026-03-23 17:01:02 +0100

    CoD2 || Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative. If the array is empty, return 0.

---
+++ Lessons/CoD2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CoD2
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            double SumArr(double[] arr)
+            {         
+                if (arr == null || arr.Length == 0) return 0;
+                double sum = 0;
+
+                foreach (var el in arr) sum += el;
+                return sum;
+            }
+
+            double[] arr = { 1, 5.2, 4, 0, -1 };
+            double[] arr1 = { -2.398 };
+            double[] arr2 = null; 
+            Console.WriteLine(SumArr(arr));
+            Console.WriteLine(SumArr(arr1));
+            Console.WriteLine(SumArr(arr2));
+
+        }
+    }
+}
