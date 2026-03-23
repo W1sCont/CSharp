42085fe6391029e46a4c37d6fc6b329dabeabfac
W1sCont <tmrostik@gmail.com>
2026-03-23 17:50:20 +0100

    Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.

---
+++ Lessons/CoD3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CoD2
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            char GradeBook(uint a, uint b, uint c)
+            {
+                uint aver = (a + b + c) / 3;
+                if (aver >= 90) return 'A';
+                if (aver >= 80) return 'B';
+                if (aver >= 70) return 'C';
+                if (aver >= 60) return 'D';
+                return 'F';
+            }
+
+            uint a = 72;
+            uint b = 78;
+            uint c = 82;
+
+            Console.WriteLine(GradeBook(a, b, c));
+
+        }
+    }
+}
