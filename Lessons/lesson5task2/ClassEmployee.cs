b7da68913a355bc71c2f1c7ccd5522559ac1446a
W1sCont <tmrostik@gmail.com>
2026-03-24 21:01:29 +0100

    lesson 5 task 2

---
+++ Lessons/lesson5task2/ClassEmployee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson5task2
+{
+    internal class ClassEmployee
+    {
+        public string? FullName {  get; set; }
+        public string? DateOfBirthday { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Position { get; set; }
+        public string? Duties { get; set; }
+
+        public ClassEmployee()
+        {
+            FullName = "Ivan Franko";
+            DateOfBirthday = "27 серпня 1856";
+            PhoneNumber = "+3800000000";
+            Email = "ivanko27@urkaine.com";
+            Position = "поет";
+            Duties = "Прозаїк, драматург, літературний критик, публіцист, перекладач, науковець, громадський і політичний діяч. Доктор філософії";
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"ПІБ: {FullName}, Дата народження: {DateOfBirthday}, Номер телефону: {PhoneNumber}, Імейл: {Email},\n" +
+                $" Посада: {Position}, Відповідальність: {Duties} \n");
+        }
+    }
+}
---
+++ Lessons/lesson5task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using lesson5task2;
+namespace Task1
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                var Michael = new ClassEmployee();
+                Michael.Info();
+
+                Michael.FullName = "Грушевський Михайло Сергійович";
+                Michael.DateOfBirthday = "29 вересня 1866";
+                Michael.PhoneNumber = "1234567890";
+                Michael.Email = "michael_freedom@ukraine.com";
+                Michael.Position = "Український історик, громадський і політичний діяч";
+                Michael.Duties = "автор понад 2000 наукових праць";
+
+                Michael.Info();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
