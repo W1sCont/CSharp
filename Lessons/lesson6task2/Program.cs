10ae8378c5ef3c94407b6470c2b51b6bad2802d1
W1sCont <tmrostik@gmail.com>
2026-03-29 18:50:01 +0200

    lesson 6 task 2

---
+++ Lessons/lesson6task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using lesson6task2;
+class MainClass
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        try
+        {
+            ListOfBooks myLibrary = new ListOfBooks();
+            myLibrary.Add(new Book("Kobzar", "Shevchenko", 1840));
+
+            Book richter = new Book(); 
+            myLibrary += richter;
+
+            Console.WriteLine($"Кількість книг: {myLibrary.Count}");
+            myLibrary.Print();
+
+            Console.WriteLine("Видаляємо Ріхтера");
+            myLibrary -= richter;
+
+            myLibrary.Print();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
