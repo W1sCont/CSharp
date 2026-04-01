59dc34522634959e62d07b7d498fa35c0802a5ad
W1sCont <tmrostik@gmail.com>
2026-04-01 18:51:19 +0200

    lesson 8 task 1

---
+++ Lessons/lesson8task1/ClassEmployee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson8task1
+{
+    internal class ClassEmployee
+    {
+        public string? FullName {  get; set; }
+        public string? DateOfBirthday { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Position { get; set; }
+        public string? Duties { get; set; }
+
+        public ClassEmployee()
+        {
+            FullName = "Ivan Franko";
+            DateOfBirthday = "27 серпня 1856";
+            PhoneNumber = "+3800000000";
+            Email = "ivanko27@urkaine.com";
+            Position = "поет";
+            Duties = "Прозаїк, драматург, літературний критик, публіцист, перекладач, науковець, громадський і політичний діяч. Доктор філософії";
+        }
+
+        public ClassEmployee(string? name)
+        {
+            FullName = name;
+            DateOfBirthday = "none";
+            PhoneNumber = "none";
+            Email = "none";
+            Position = "none";
+            Duties = "none";
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"ПІБ: {FullName}, Дата народження: {DateOfBirthday}, Номер телефону: {PhoneNumber}, Імейл: {Email},\n" +
+                $" Посада: {Position}, Відповідальність: {Duties}\n");
+        }
+
+        public (string? type, double salary) CalculateSalary(double baseRate)
+        {
+            return ("фіксована ставка ", baseRate);
+        }
+
+        public (string? type, double salary) CalculateSalary(double hourlyRate, int hoursWorked)
+        {
+            double total = hourlyRate * hoursWorked;
+            return ($"погодинна оплата ({hoursWorked} год.) ", total);
+        }
+
+        public (string? type, double salary) CalculateSalary(double baseRate, double bonusPercentage)
+        {
+            double total = baseRate + (baseRate * bonusPercentage / 100);
+            return ($"ставка з урахуванням премії {bonusPercentage}% ", total);
+        }
+        public static void PrintInfo(string name, (string note, double salary) data)
+        {
+            Console.WriteLine($"Працівник: {name}");
+            Console.WriteLine($"Нараховано: {data.salary:F2} грн");
+            Console.WriteLine($"Примітка: {data.note}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lessons/lesson8task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using lesson8task1;
+namespace Task1
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                var Michael = new ClassEmployee();
+                Michael.Info();
+
+                Michael.FullName = "Грушевський Михайло Сергійович";
+                Michael.DateOfBirthday = "29 вересня 1866";
+                Michael.PhoneNumber = "1234567890";
+                Michael.Email = "michael_freedom@ukraine.com";
+                Michael.Position = "Український історик, громадський і політичний діяч";
+                Michael.Duties = "автор понад 2000 наукових праць";
+                Michael.Info();
+
+                Console.WriteLine("\nlesson 8 taks 1\n");
+                ClassEmployee emp1 = new("Олександр Коваленко");
+                ClassEmployee emp2 = new("Марія Петренко");
+                ClassEmployee emp3 = new("Іван Сірко");
+
+                Console.WriteLine("Демонстрація фіксованої ставки");
+                var res1 = emp1.CalculateSalary(15000);
+                ClassEmployee.PrintInfo(emp1.FullName, res1);
+
+                Console.WriteLine("Демонстрація погодинної оплати (наприклад, 200 грн/год, відпрацьовано 160 год)");
+                var res2 = emp2.CalculateSalary(200, 160);
+                ClassEmployee.PrintInfo(emp2.FullName, res2);
+
+                Console.WriteLine("Демонстрація премії (ставка 12000 + 25% премії)");
+                var res3 = emp3.CalculateSalary(12000, 25.0);
+                ClassEmployee.PrintInfo(emp3.FullName, res3);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
