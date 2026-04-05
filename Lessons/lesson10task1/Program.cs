2f6be8e9af97c41f0e89999283a4730886ab3a49
W1sCont <tmrostik@gmail.com>
2026-04-05 20:31:11 +0200

    lesson 10 task 1

---
+++ Lessons/lesson10task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using lesson10task1;
+namespace CSharpApplication.Lesson10
+{ 
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                Console.WriteLine("Введіть дані для запису: ");
+                fn10 result = new fn10();
+                string? str;
+                double temp = 0;
+                
+                while (true)
+                {
+                    str = Console.ReadLine();
+                    if (str == null) break;
+                    temp = Convert.ToDouble(str);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
---
+++ Lessons/lesson10task1/fn10.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace lesson10task1
+{
+    internal class fn10
+    {
+        private int count = 0;
+        private int capasity = 10;
+        private double[] arr;
+
+        public fn10() { arr = new double[Capasity]; }
+
+        public int Count { get { return count; } }
+        public int Capasity { get { return capasity; } }
+        private void SetCapasity(int value) { capasity += value;}
+
+        public void AddToArr(double num)
+        {
+            if (Count == Capasity) SetCapasity(10);
+            arr[count] = num;
+            count++;
+        }
+
+        public fn10 this[double num] { set { AddToArr(num); } }
+        public void SaveToFile(string pass)
+        {
+            StreamWriter fs = new StreamWriter(pass, true);
+            for (int i = 0; i < count; i++) { fs.Write(arr[i] + ","); }
+            fs.Close();
+        }
+
+        public void LoadFromFile(string pass)
+        {
+            StreamReader fs = new StreamReader(pass);
+        }
+    }
+}
