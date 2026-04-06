88dde57bd9857b10a9955551ddbafa9c1a80a0b3
W1sCont <tmrostik@gmail.com>
2026-04-06 15:48:57 +0200

    lesson 10 task 3

---
+++ Lessons/lesson10task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using lesson10task3;
+namespace CSharpApplication.DataTypes
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                Console.WriteLine("Введіть шлях до файлу: ");
+                string? filename = Console.ReadLine();
+                Run start = new();
+                start.RunStatistics(filename);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
