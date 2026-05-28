6d83730a22654ce58d766bee9a06f06f2d9cc8fc
Ricks <tmrostik@gmail.com>
2026-05-28 17:25:09 +0200

    Generics-колекції. Dictionary<T>

--- lesson20task1/DicT.cs
+++ lesson20task1/DicT.cs
@@ -1,84 +1,73 @@
+using System.Collections;
+
 namespace lesson20task1;
 
-public class DicT<T, V>
+public class DicT<T, V> : IEnumerable<KeyValuePair<T, List<V>>>
 {
-    private Dictionary<T, V> dict = new Dictionary<T, V>();
+    private Dictionary<T, List<V>> dict = new Dictionary<T, List<V>>();
 
     public void Add(T key, V value)
     {
         if (!dict.ContainsKey(key))
         {
-            dict.Add(key, value);
-            Console.WriteLine($"Word {key} added");
-        }else
+            dict.Add(key, new List<V>{value});
+        }
+        else
         {
-            Console.WriteLine($"This word is already in the dictionary");
+            if (!dict[key].Contains(value))
+            {
+                dict[key].Add(value);
+            }
         }
     }
 
     public void Dell(T key)
     {
-        if (dict.ContainsKey(key))
-        {
-            dict.Remove(key);
-            Console.WriteLine($"Word {key} deleted");
-        }
-        else
-        {
-            Console.WriteLine($"Word {key} not found");
-        }
+        if (dict.ContainsKey(key)) { dict.Remove(key); }
     }
     
     public void DellTranslate(T key)
     {
-        if (dict.ContainsKey(key))
-        {
-            dict[key] = default(V)!; 
-            Console.WriteLine($"Translation for {key} cleared");
-        }
-        else
-        {
-            Console.WriteLine($"Word {key} not found");
-        }
+        if (dict.ContainsKey(key)) { dict[key].Clear(); }
     }
 
     public void EditWord(T key, T newKey)
     {
         if (dict.ContainsKey(key) && !dict.ContainsKey(newKey))
         {
-            V temp = dict[key];
+            List<V> temp = dict[key];
             dict.Remove(key);
             dict.Add(newKey, temp);
-            Console.WriteLine($"Word edited");
-        }
-        else
-        {
-            Console.WriteLine($"Word {key} not found");
         }
     }
 
     public void Edit(T key, V value)
     {
-        if (dict.ContainsKey(key))
-        {
-            dict[key] = value;
-            Console.WriteLine($"Translate edited");
-        }
-        else
-        {
-            Console.WriteLine($"Word {key} not found");
-        }
+        if (dict.ContainsKey(key)) { dict[key] = new List<V>{value}; }
     }
     
-    public void Find(T key)
+    public bool Find(T key)
     {
-        if (dict.ContainsKey(key))
+        if (dict.ContainsKey(key)) { return true; }
+        return false;
+    }
+
+    public IEnumerator<KeyValuePair<T, List<V>>> GetEnumerator()
+    {
+        return dict.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    public List<V> this[T index]
+    {
+        get
         {
-            Console.WriteLine($"Translate of {key} is {dict[key]}");
+            if (Find(index)) return dict[index]; 
+            throw new KeyNotFoundException();
         }
-        else
-        {
-            Console.WriteLine($"Word {key} not found");
-        }
+        // set { dict[index] = value; }
     }
 }
--- lesson20task1/Program.cs
+++ lesson20task1/Program.cs
@@ -10,17 +10,125 @@ namespace Program_20
 
             try
             {
-                DicT<string, string> ditc = new DicT<string, string>();
-                ditc.Add("Hi", "Bonjour");
-                ditc.Add("Apple", "Pomme");
-                ditc.Add("Book", "Livre");
-                ditc.Add("Cat", "Chat");
-                ditc.Add("House", "Maison");
-                ditc.Add("Water", "Eau");
-                ditc.Add("Friend", "Ami / Amie");
-                ditc.Add("Sun", "Soleil");
-                ditc.Add("Night", "Nuit");
-                ditc.Add("Work", "Travail");
+                DicT<string, string> dict = new DicT<string, string>();
+                bool stop = true;
+                while (stop)
+                {
+                    foreach (var k in dict)
+                    {
+                        Console.Write(k.Key + " - ");
+                        Console.Write(string.Join(", ", k.Value));
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("1-Add word and translate, 2-Remove word, 3-Remove translate, 4-Edit word, 5-Edit translate, 6-Find, 0-Exit");
+
+                    string? choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            Console.WriteLine("Enter word:");
+                            string? word = Console.ReadLine();
+                            Console.WriteLine("Enter translate:");
+                            string? translate = Console.ReadLine();
+                            if (word != null && translate != null)
+                            {
+                                dict.Add(word, translate);
+                                Console.WriteLine($"Word {word} added");
+                            }
+                            else { Console.WriteLine("Field cant be empty!"); }
+                            break;
+                        case "2":
+                            Console.WriteLine("Enter word:");
+                            word = Console.ReadLine();
+                            if (dict.Find(word))
+                            {
+                                dict.Dell(word);
+                                Console.WriteLine($"Word {word} deleted");
+                            }
+                            else { Console.WriteLine($"Word {word} not found"); }
+                            break;
+                        case "3":
+                            Console.WriteLine("Enter word:");
+                            word = Console.ReadLine();
+                            if (dict.Find(word))
+                            {
+                                dict.DellTranslate(word);
+                                Console.WriteLine($"Translate of {word} is deleted");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Word {word} not found");
+                            }
+                            break;
+                        case "4":
+                            Console.WriteLine("Enter word:");
+                            word = Console.ReadLine();
+                            if (dict.Find(word))
+                            {
+                                Console.WriteLine("Enter new word:");
+                                string? newWord = Console.ReadLine();
+                                if (newWord != null)
+                                {
+                                    dict.EditWord(word, newWord);
+                                    Console.WriteLine($"Translate of {word} edited");
+                                }
+                                else { Console.WriteLine("Field cant be empty!"); }
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Word {word} not found");
+                            }
+                            break;
+                        case "5":
+                            Console.WriteLine("Enter word:");
+                            word = Console.ReadLine();
+                            if (dict.Find(word))
+                            {
+                                Console.WriteLine("Enter new word:");
+                                string? newWord = Console.ReadLine();
+                                if (newWord != null)
+                                {
+                                    dict.Edit(word, newWord);
+                                    Console.WriteLine($"Translate edited");
+                                }
+                                else { Console.WriteLine("Field cant be empty!"); }
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Word {word} not found");
+                            }
+                            break;
+                        case "6":
+                            Console.WriteLine("Enter word:");
+                            word = Console.ReadLine();
+                            if (dict.Find(word))
+                            {
+                                Console.WriteLine($"Translate of {word} is {dict[word]}");
+                                dict.Dell(word);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Word {word} not found");
+                            }
+                            break;
+                        case "0":
+                            stop = false;
+                            break;
+                    }
+                }
+                
+                /*
+                str.Add("Bonjour");
+                ditc.Add("Hi", str);
+                ditc.Add("Apple", new List<string>(){"Pomme"});
+                ditc.Add("Book", new List<string>(){"Livre"});
+                ditc.Add("Cat", new List<string>(){"Chat"});
+                ditc.Add("House", new List<string>(){"Maison"});
+                ditc.Add("Water", new List<string>(){"Eau"});
+                ditc.Add("Friend", new List<string>(){"Ami", "Amie"});
+                ditc.Add("Sun", new List<string>(){"Soleil"});
+                ditc.Add("Night", new List<string>(){"Nuit"});
+                ditc.Add("Work", new List<string>(){"Travail"});
                 // Love - Amour
                 ditc.Find("Hi");
                 ditc.Edit("Hi", "Salut");
@@ -32,6 +140,7 @@ namespace Program_20
                 ditc.Find("Hello");
                 ditc.Dell("Hello");
                 ditc.Find("Hello");
+                */
             }
             catch (Exception ex)
             {
